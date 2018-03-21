8f658ae9441849d93b4e4237e5a1d085ce33df75
wytriam <thomas.m.frick@gmail.com>
2018-03-21 15:00:39 -0400

    cleaned some things up

--- clear-screen.asm
+++ clear-screen.asm
@@ -1,5 +1,5 @@
 ; Example
-; Name: 				Thomas Frick
+; Name: 				Thomas Frick & Bobby Clarahan
 ; Course: 				COMP 252
 ; Instructor: 			Dr. Conlon
 ; Date started: 		February 3, 2015
@@ -12,52 +12,38 @@
 ; Define some constants
 space 	= $20				; ASCII code for space.
 home	= $7000				;Address of upper left (home) on video screen ; .DW means we use a 2-byte value
-statln	= home				;24*40+home		;First space on stat line (bottom line) 
+;statln	= home				;24*40+home		;First space on stat line (bottom line) 
 
 	.OR		$0000			;Start code at address $0000
 	jmp		start			;$0300 - Assembler has 2 passes, so we can use variables before we define them if they get defined somewhere in the program	;Jump to the beginning of the program, proper.
 							; jmp is 4C
 	
 ; Define zero-page storage
-msgptr 	.BS 2				; Pointer to string to print. Belongs to prtmsg
-msg		.AZ		" "			;
+curLn	.DW		home		; creates a variable to store the current line and starts it on home
+linLn	.DW		40			; line length (40)
 		.BS		$0300-*		;Skip to the beginning of the program, proper.
 							; * means the current instruction in the program counter
 							; .BS stands for byte string. This sets aside everything between the variable and $0300
 		
 start	cld					;Set binary mode. (clear decimal mode) clear decimal is D8
-		lda #msg			;Get parameter
-		pha					;Push it into the stack
-		jsr .prtmsg			;jsr - jump to subroutine; prtmsg - go and print a message
+		jsr .clrsrn			;jsr - jump to subroutine; clrsrn - clear screen
 		brk					;And stop.
 		
-.prtmsg	pla					;Get return address
-		sta .retadr
-		pla
-		sta .retadr+1
-		pla
-		sta .stradr
-		lda #0				;Initialize index registers.
+.clrsrn	lda #0				;Initialize index registers.
 		tay
 		tax
-.loop	lda msg,x			;Get next character to print.
-		cmp #0
-		beq .out
-		sta statln,y
+		lda #space			;Fill rest of line with spaces
+.nxtLn	ldy #0
+.loop	sta (curLn),y		;Print the space
+		iny
+		cpy linLn				;End of line?
+		bmi .loop
+		; update the curLn variable
+		adc (curLn)
+		adc (curLn)
 		inx
-		iny
-		jmp .loop
-.out	lda #space			;Fill rest of line with spaces
-.loop2	sta statln,y		;Print the space
-		iny
-		cpy #40				;End of line?
-		bmi .loop2
-		lda .retadr+1		;Restore return address to stack
-		pha
-		lda .retadr
-		pha
+		cpx #24
+		bmi .nxtLn
 		rts
-.retadr .BS 2
-.stradr .BS 1
 		.EN					;End of program. Might not be required, but doesn't hurt. Any code below this isn't assembled. 
 		
