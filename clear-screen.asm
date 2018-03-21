00dd717843aaf7fc7955404696d16e9cdd1a5685
wytriam <thomas.m.frick@gmail.com>
2018-03-21 17:51:43 -0400

    clear screen works

--- clear-screen.asm
+++ clear-screen.asm
@@ -12,7 +12,6 @@
 ; Define some constants
 space 	= $20				; ASCII code for space.
 home	= $7000				;Address of upper left (home) on video screen ; .DW means we use a 2-byte value
-;statln	= home				;24*40+home		;First space on stat line (bottom line) 
 
 	.OR		$0000			;Start code at address $0000
 	jmp		start			;$0300 - Assembler has 2 passes, so we can use variables before we define them if they get defined somewhere in the program	;Jump to the beginning of the program, proper.
@@ -24,12 +23,14 @@ linLn	.DW		40			; line length (40)
 		.BS		$0300-*		;Skip to the beginning of the program, proper.
 							; * means the current instruction in the program counter
 							; .BS stands for byte string. This sets aside everything between the variable and $0300
-		
+
+;main code							
 start	cld					;Set binary mode. (clear decimal mode) clear decimal is D8
-		jsr .clrsrn			;jsr - jump to subroutine; clrsrn - clear screen
+		jsr clrsrn			;jsr - jump to subroutine; clrsrn - clear screen
 		brk					;And stop.
-		
-.clrsrn	ldx #0				;Initialize index registers.
+
+;sub-routine clear screen		
+clrsrn	ldx #0				;Initialize index registers.
 		lda #space			
 .nxtLn	ldy #0
 .loop	sta (curLn),y		;Print the space
@@ -37,13 +38,24 @@ start	cld					;Set binary mode. (clear decimal mode) clear decimal is D8
 		cpy linLn				;End of line?
 		bmi .loop
 		; update the curLn variable (increment by 40)
-		lda #40
-		adc curLn ;make the accumulator equal to curLn + 40
-		sta curLn ;put that value into curLn, meaning curLn = curLn + 40
-		lda #space ; reset the accumulator
+		lda curLn
+		add #40
+		sta curLn
+		; deal with over flow
+		bcc .cont
+		lda curLn+1
+		add #01
+		sta curLn+1
+.cont	lda #space
+		;lda #40
+		;adc curLn ;make the accumulator equal to curLn + 40
+		;sta curLn ;put that value into curLn, meaning curLn = curLn + 40
+		;lda #space ; reset the accumulator
 		inx
-		cpx #24
+		cpx #25
 		bmi .nxtLn
 		rts
+		
+		
 		.EN					;End of program. Might not be required, but doesn't hurt. Any code below this isn't assembled. 
 		
