116a5f9f2cd25211196f16fb417b96d018510adc
wytriam <thomas.m.frick@gmail.com>
2018-03-21 21:46:49 -0400

    everything for deliverable 1 is ready

--- input.asm
+++ input.asm
@@ -13,29 +13,29 @@ iostat	=	iobase+1	;No spaces in sbasm expressions
 iocmd	=	iobase+2
 ioctrl	=	iobase+3
 
-	.OR		$0300
+	.OR		$0000			;Start code at address $0000
+	jmp		start			;$0300 - Assembler has 2 passes, so we can use variables before we define them if they get defined somewhere in the program	;Jump to the beginning of the program, proper.
 
-start	cli
-		lda #$0b
+	.BS		$0300-*		;Skip to the beginning of the program, proper.
+
+	
+start	cld
+		cli				; Clear interrupt disable bit
+		lda #$0b		
 		sta iocmd		; Set command status
 		lda #$1a
 		sta ioctrl		; 0 stop bits, 8 bit word, 2400 baud
-		
-	;; Load a character from the keyboard and store it into 
-	;; the accumulator
-	
 getkey	lda	iostat		; Read the ACIA status
 		and #$08		; Is the rx register empty?
 		beq	getkey		; Yes, wait for it to fill
 		lda	iobase		; Otherwise, read into accumulator
-
-;; Write the current char in the accumulator to the console
-
+		sta $7000
 write	pha				; Save accumulator
 write1	lda	iostat		; Read the ACIA status
 		and #$10		; Is the tx register empty?
 		beq	write1		; No, wait for it to empty
 		pla				; Otherwise, load saved accumulator
 		sta	iobase		; and write to output.
-		
 		jmp getkey		; Repeat
+		
+		.EN
