796ec327fc931425dcb7b7840c2d9de114847c29
wytriam <thomas.m.frick@gmail.com>
2018-04-07 17:56:04 -0400

    puck isn't drawing; commented out math to focus on that

--- breakout.asm
+++ breakout.asm
@@ -38,8 +38,7 @@ ySign	.DB	0	; The sign of the deltaX. 0 is positive, 1 is negative
 ;main code							
 start	cld		;Set binary mode. (clear decimal mode) clear decimal is D8
 	cli		;Clear interrupt disable bit
-	jsr clrsrn	;jsr - jump to subroutine; clrsrn - clear screen
-			
+	jsr clrsrn	;jsr - jump to subroutine; clrsrn - clear screen			
 main	jsr drwPuk	;main redraws the puck and checks for player input
 	lda #$0b		
 	sta iocmd	;Set command status
@@ -86,74 +85,74 @@ clrsrn	ldx #0		;Initialize index registers.
 ;
 drwPuk	lda #space	; Prepare to clear screen where puck was
 	sta (pukPos)	; Clear screen where puck was
-	ldx #0		; set x counter to 0 (initialize for movement)
-	ldy #0		; set y counter to 0 (initialize for movement)
-.loop	cpx (deltaX)	; loop for moving the puck. this makes sure we don't move past deltaX
-	bne .doneX	;.doneX is for when we've processed the X movement this cycle
-	inx		; we're processing this x, so increment it. 
-	lda (xSign)	; check the direction of deltaX
-	cmp #$00	; this checks if xSign is positive
-	beq .xPos
-	clc		;x is negative
-	lda pukPos	;update the pukPos variable (subtract 1)
-	sbc #01		;decrement by 1
-	sta pukPos
-	lda pukPos+1	;deal with over flow
-	sbc #00
-	sta pukPos+1	
-.xPos	clc		;Clear the carry flag
-	lda pukPos	;update the pukPos variable (increment by 1)
-	adc #01		;we can't use inc because we might have overflow
-	sta curLn
-	lda curLn+1	;deal with over flow
-	adc #00
-	sta curLn+1
-	jsr openPos	;check whether the new location is free
-	cmp #$01	;is this space open? 01 is yes
-	beq .doneX	;ends here if a = $01
-	dex		; we don't want this x to count. undo it. 
-	cmp (deltaX)	;a = 00; we need to flip the xSign. First, check what xSign is
-	beq .flipX	
-	lda #$00	;xSign = 01; Set it to 00
-	sta deltaX	;save the new deltaX
-	jmp .doneX	;we're finished with x
-.flipX	lda #$01	;xSign = 00; Set it to 01
-	sta deltaX	;save the new deltaX
-.doneX	cpy (deltaY)
-	bne .doneY
-	iny		; we're processing this y, so increment it. 
-	lda (ySign)	; check the direction of deltaY
-	cmp #$00	; this checks if ySign is positive
-	beq .yPos
-	clc		;y is negative
-	lda pukPos	;update the pukPos variable (subtract 40)
-	sbc #40		;decrement by 40
-	sta pukPos
-	lda pukPos+1	;deal with over flow
-	sbc #00
-	sta pukPos+1	
-.yPos	clc		;Clear the carry flag
-	lda pukPos	;update the pukPos variable (add 40)
-	adc #40		;we can't use inc because we might have overflow
-	sta curLn
-	lda curLn+1	;deal with over flow
-	adc #00
-	sta curLn+1
-	jsr openPos	;check whether the new location is free
-	cmp #$01	;is this space open? 01 is yes
-	beq .doneY	;ends here if a = $01
-	dey		; we don't want this y to count. undo it. 
-	cmp (deltaY)	;a = 00; we need to flip the ySign. First, check what ySign is
-	beq .flipY	
-	lda #$00	;ySign = 01; Set it to 00
-	sta deltaY	;save the new deltaY
-	jmp .doneY	;we're finished with y
-.flipY	lda #$01	;xSign = 00; Set it to 01
-	sta deltaY	;save the new deltaY
-.doneY	cpx (deltaX)	; compare the x-counter to deltaX
-	bmi .loop	; loop back if x needs more iterations
-	cpy (deltaY)	; compare the y-counter to deltaY
-	bmi .loop
+;	ldx #0		; set x counter to 0 (initialize for movement)
+;	ldy #0		; set y counter to 0 (initialize for movement)
+;.loop	cpx (deltaX)	; loop for moving the puck. this makes sure we don't move past deltaX
+;	bne .doneX	;.doneX is for when we've processed the X movement this cycle
+;	inx		; we're processing this x, so increment it. 
+;	lda (xSign)	; check the direction of deltaX
+;	cmp #$00	; this checks if xSign is positive
+;	beq .xPos
+;	clc		;x is negative
+;	lda pukPos	;update the pukPos variable (subtract 1)
+;	sbc #01		;decrement by 1
+;	sta pukPos
+;	lda pukPos+1	;deal with over flow
+;	sbc #00
+;	sta pukPos+1	
+;.xPos	clc		;Clear the carry flag
+;	lda pukPos	;update the pukPos variable (increment by 1)
+;	adc #01		;we can't use inc because we might have overflow
+;	sta curLn
+;	lda curLn+1	;deal with over flow
+;	adc #00
+;	sta curLn+1
+;	jsr openPos	;check whether the new location is free
+;	cmp #$01	;is this space open? 01 is yes
+;	beq .doneX	;ends here if a = $01
+;	dex		; we don't want this x to count. undo it. 
+;	cmp (deltaX)	;a = 00; we need to flip the xSign. First, check what xSign is
+;	beq .flipX	
+;	lda #$00	;xSign = 01; Set it to 00
+;	sta deltaX	;save the new deltaX
+;	jmp .doneX	;we're finished with x
+;.flipX	lda #$01	;xSign = 00; Set it to 01
+;	sta deltaX	;save the new deltaX
+;.doneX	cpy (deltaY)
+;	bne .doneY
+;	iny		; we're processing this y, so increment it. 
+;	lda (ySign)	; check the direction of deltaY
+;	cmp #$00	; this checks if ySign is positive
+;	beq .yPos
+;	clc		;y is negative
+;	lda pukPos	;update the pukPos variable (subtract 40)
+;	sbc #40		;decrement by 40
+;	sta pukPos
+;	lda pukPos+1	;deal with over flow
+;	sbc #00
+;	sta pukPos+1	
+;.yPos	clc		;Clear the carry flag
+;	lda pukPos	;update the pukPos variable (add 40)
+;	adc #40		;we can't use inc because we might have overflow
+;	sta curLn
+;	lda curLn+1	;deal with over flow
+;	adc #00
+;	sta curLn+1
+;	jsr openPos	;check whether the new location is free
+;	cmp #$01	;is this space open? 01 is yes
+;	beq .doneY	;ends here if a = $01
+;	dey		; we don't want this y to count. undo it. 
+;	cmp (deltaY)	;a = 00; we need to flip the ySign. First, check what ySign is
+;	beq .flipY	
+;	lda #$00	;ySign = 01; Set it to 00
+;	sta deltaY	;save the new deltaY
+;	jmp .doneY	;we're finished with y
+;.flipY	lda #$01	;xSign = 00; Set it to 01
+;	sta deltaY	;save the new deltaY
+;.doneY	cpx (deltaX)	; compare the x-counter to deltaX
+;	bmi .loop	; loop back if x needs more iterations
+;	cpy (deltaY)	; compare the y-counter to deltaY
+;	bmi .loop
 	lda #pukChar	; Prepare to re-draw the puck
 	sta (pukPos)	; Re-draw the puck;
 	rts
