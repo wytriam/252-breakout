c44fd1dc0bd12f09aef3cf3ba7118335517e1429
wytriam <thomas.m.frick@gmail.com>
2018-04-07 16:23:33 -0400

    updated screen clear to not use ADD (uses ADC now), started working on making puck move

--- breakout.asm
+++ breakout.asm
@@ -26,8 +26,11 @@ ioctrl	= iobase+3
 curLn	.DW	home	;creates a variable to store the current line and starts it on home
 linLn	.DW	40	;line length (40)
 pukPos 	.DW	$71CC	;the location of the puck
-delYP	.DB	0	; The vertical change in the puck. Ranges from -4 to 4. 
-delXP	.DB	0	; The horizontal change in the puck. Ranges from -4 to 4. 
+deltaX	.DB	0	; The vertical change in the puck.
+xSign	.DB	0	; The sign of the deltaX. 0 is positive, 1 is negative
+deltaY	.DB	0	; The horizontal change in the puck.
+ySign	.DB	0	; The sign of the deltaX. 0 is positive, 1 is negative
+
 	.BS	$0300-*	;Skip to the beginning of the program, proper.
 			;* means the current instruction in the program counter
 			;.BS stands for byte string. This sets aside everything between the variable and $0300
@@ -66,26 +69,47 @@ clrsrn	ldx #0		;Initialize index registers.
 	iny
 	cpy linLn	;End of line?
 	bmi .loop
+	clc		;Clear the carry flag
 	lda curLn	;update the curLn variable (increment by 40)
-	add #40
+	adc #40
 	sta curLn
-	bcc .cont		
 	lda curLn+1	;deal with over flow
-	add #01
+	adc #00
 	sta curLn+1
-.cont	lda #space
+	lda #space
 	inx
 	cpx #25
 	bmi .nxtLn
 	rts
 ;
-; sub-routine to draw puck
+; sub-routine to draw the puck
 ;
 drwPuk	lda #space	; Prepare to clear screen where puck was
 	sta (pukPos)	; Clear screen where puck was
-			; Movement yayayayaya
+	ldx #0		; set x counter to 0 (initialize for movement)
+	ldy #0		; set y counter to 0 (initialize for movement)
+.loop	cpx (deltaX)	; loop for moving the puck. this compares the 
+	bne .doneX
+.doneX	cpy (deltaY)
+	bne .doneY
+.doneY	cpx (deltaX)	; compare the x-counter to deltaX
+	bmi .loop	; loop back if x needs more iterations
+	cpy (deltaY)	; compare the y-counter to deltaY
+	bmi .loop
 	lda #pukChar	; Prepare to re-draw the puck
 	sta (pukPos)	; Re-draw the puck;
 	rts
 	
+;
+; sub-routine that checks to see if the position of pukPos is open or not. If the position == space, sets a to $01
+;
+openPos	lda #space	; Prepare to compare whatever is in memory at pukPos to space
+	cmp (pukPos)	; compare a to whatever is in memory locaton pukPos
+	beq .true	; a and space are the same. branch
+	lda #$00	; a != space. return false
+	rts
+.true	lda #$01	; return true
+	rts
+
+
 	.EN		;End of program. Might not be required, but doesn't hurt. Any code below this isn't assembled. 		
