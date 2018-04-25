0c84c488bd3d40578b38b07812424d0e0581a581
wytriam <thomas.m.frick@gmail.com>
2018-04-24 21:40:23 -0400

    onScrn, bounce pass all unit tests. Refactored xPuck, yPuck to pkRow, pkCol respectively.

--- breakout2.asm
+++ breakout2.asm
@@ -23,114 +23,118 @@ false 	= 00
 
 ; VARIABLES
 curLine	.DW home	;creates a variable to store the current line that is 2 bytes large
-xPuck	.DB 12		;the x position of the puck
-deltaX	.DB $01		;the change in x position
-xSign	.DB $00		;the positive/negative sign of deltaX. 01 is positive, 00 is negative
-xCnt	.DB 00		;
-yPuck	.DB 20		;the y position of the puck
-deltaY	.DB $01		;the change in y position
-ySign	.DB $01		;the positive/negative sign of deltaY. 01 is positive, 00 is negative
-yCnt	.DB 00		;
+pkRow	.DB $0B		;the row the puck is on. Ranges from 0-23 ($00-$17 hex)
+;deltaX	.DB $01		;the change in x position
+;xSign	.DB $00		;the positive/negative sign of deltaX. 01 is positive, 00 is negative
+;xCnt	.DB 00		;
+pkCol	.DB $13		;the column the puck is on. Ranges from 0-39 ($00-$27 hex)
+;deltaY	.DB $01		;the change in y position
+;ySign	.DB $01		;the positive/negative sign of deltaY. 01 is positive, 00 is negative
+;yCnt	.DB 00		;
 	.BS $0300-*	;Skip to the beginning of the program, proper.
 
 	
 start	cld		;Set binary mode. (clear decimal mode)
 	jsr init	;initialize the game
-.main	jsr movePk
-	jmp .main
+;.main	jsr movePk
+;	jmp .main
 	
 	brk		;Stop the program
 	
 ;
 ; sub-routine to initialize the game
+; Parameters: none
+; Return: none
 ;	
 init	jsr clrScrn	;clear the screen
 	jsr crsrOff	;turn the cursor off
-	lda #puck	; set the char for the ball ('o')
-	pha		; turn that parameter in
-	lda xPuck	; set the row to 12
+	lda #puck	;set the char for the ball ('o')
+	pha		;turn that parameter in
+	lda pkRow	;store the row parameter
 	pha
-	lda yPuck	; set the col to 20
+	lda pkCol	;store the col parameter
 	pha
-	jsr printC	; print the ball
-	jsr movePk	; move the ball once
+	jsr printC	;print the ball
+	;jsr movePk	;move the ball once
 	rts
 
 ;
-;sub-routine to move the puck
+; sub-routine to move the puck
+; Parameters: none
+; Return: none
 ;
-movePk	stx .xReg	;save the contents of the x-register
-	sty .yReg	;save the contents of the y-register
-	lda #space	; clear the current puck Pos
-	pha		; turn that parameter in
-	lda xPuck	; set the row to xPuck
-	pha
-	lda yPuck	; set the col to yPuck
-	pha
-	jsr printC	; print a space
-	; move the ball appropriately
-	ldx deltaX	;
-	stx xCnt
-	ldy deltaY	;
-	sty yCnt
-.mvmtLp	ldx xCnt
-	cpx #00
-	beq .doneX
-	dex		; still x movement to process
-	lda xSign
-	cmp #01
-	beq .xPlus
-	dec xPuck	; x is negative
-	jmp .chkX
-.xPlus	inc xPuck
-.chkX	stx xCnt
-	jsr bounce
-	pla
-	cmp #01
-	beq .togX	; x is positive, set xSign to 00
-	lda #01
-	sta xSign
-	jmp .doneX
-.togX	lda #00
-	sta xSign
-.doneX	ldy yCnt
-	cpy #00
-	beq .doneY
-	dey		; still y movement to process
-	lda ySign
-	cmp #01
-	beq .yPlus
-	dec yPuck	; y is negative
-	jmp .chkY
-.yPlus	inc yPuck
-.chkY	sty yCnt
-	jsr bounce
-	pla
-	cmp #01
-	beq .togY	; x is positive, set xSign to 00
-	lda #01
-	sta ySign
-	jmp .doneY
-.togY	lda #00
-	sta ySign
-.doneY	ldx xCnt
-	cpx #00
-	bne .mvmtLp
-	ldy yCnt
-	cpy #00
-	bne .mvmtLp
-	lda #111	; set the char for the ball
-	pha		; turn that parameter in
-	lda xPuck	; set the row to xPuck
-	pha
-	lda yPuck	; set the col to yPuck
-	pha
-	jsr printC	; print a space
-	ldx .xReg	;Restore x register
-	ldy .yReg	;Restore y register
-	rts
-.xReg	.DB 0
-.yReg	.DB 0
+;movePk	stx .xReg	;save the contents of the x-register
+;	sty .yReg	;save the contents of the y-register
+;	lda #space	;clear the current puck Pos
+;	pha		;turn that parameter in
+;	lda pkRow	;set the row to pkRow
+;	pha
+;	lda pkCol	;set the col to pkCol
+;	pha
+;	jsr printC	;print a space
+;	; move the ball appropriately
+;	ldx deltaX	;
+;	stx xCnt
+;	ldy deltaY	;
+;	sty yCnt
+;.mvmtLp	ldx xCnt
+;	cpx #00
+;	beq .doneX
+;	dex		;still x movement to process
+;	lda xSign
+;	cmp #01
+;	beq .xPlus
+;	dec pkRow	;x is negative
+;	jmp .chkX
+;.xPlus	inc pkRow
+;.chkX	stx xCnt
+;	jsr bounce
+;	pla
+;	cmp #01
+;	beq .togX	;x is positive, set xSign to 00
+;	lda #01
+;	sta xSign
+;	jmp .doneX
+;.togX	lda #00
+;	sta xSign
+;.doneX	ldy yCnt
+;	cpy #00
+;	beq .doneY
+;	dey		;still y movement to process
+;	lda ySign
+;	cmp #01
+;	beq .yPlus
+;	dec pkCol	;y is negative
+;	jmp .chkY
+;.yPlus	inc pkCol
+;.chkY	sty yCnt
+;	jsr bounce
+;	pla
+;	cmp #01
+;	beq .togY	;x is positive, set xSign to 00
+;	lda #01
+;	sta ySign
+;	jmp .doneY
+;.togY	lda #00
+;	sta ySign
+;.doneY	ldx xCnt
+;	cpx #00
+;	bne .mvmtLp
+;	ldy yCnt
+;	cpy #00
+;	bne .mvmtLp
+;	lda #111	;set the char for the ball
+;	pha		;turn that parameter in
+;	lda pkRow	;set the row to pkRow
+;	pha
+;	lda pkCol	;set the col to pkCol
+;	pha
+;	jsr printC	;print a space
+;	ldx .xReg	;Restore x register
+;	ldy .yReg	;Restore y register
+;	rts
+;.xReg	.DB 0
+;.yReg	.DB 0
 	
 
 ;
@@ -142,40 +146,25 @@ movePk	stx .xReg	;save the contents of the x-register
 bounce	stx .xReg	;save the contents of the x-register
 	sty .yReg	;save the contents of the y-register
 	pla
-	sta .save	; where does .save and .save+1 actually save? ;save the first byte of the return address
+	sta .save	;save the first byte of the return address
 	pla
-	sta .save+1	; save the second byte of the return address
-	clc
-	lda xPuck	; load the x position of the puck
-	cmp #40		; make sure x is not going too far to the right
-	bpl .rFalse
-	clc
-	lda xPuck
-	cmp #00		; make sure x is not going too far to the left
-	bmi .rFalse
-	clc
-	lda yPuck	; load the x position of the puck
-	cmp #24		; make sure y is not going too far to down
-	bpl .rFalse
-	clc
-	lda yPuck
-	cmp #00		; make sure y is not going too far to up
-	bmi .rFalse
-	lda #xPuck
+	sta .save+1	;save the second byte of the return address
+	; get what's in the location of the puck
+	lda pkRow
 	pha
-	lda #yPuck
+	lda pkCol
 	pha
 	jsr getC
-	pla
-	cmp #space	; make sure the space the puck is in is a space
-	bne .rFalse	; if it isn't, return false
-	jmp .rTrue
-.rFalse	lda #false
+	pla		;this is the puck location char
+	cmp #puck	;the ball can't bounce of off itself
+	beq .noBnc	;	this check might not be necessary after movePk works
+	cmp #space	;make sure the space the puck is in is a space
+	bne .bnc	;if it isn't, return true (so that it bounces)
+.noBnc	lda #false
 	pha
 	jmp .return
-.rTrue	lda #true
+.bnc	lda #true
 	pha
-	jmp .return
 	; return information
 .return	lda .save+1
 	pha
@@ -189,9 +178,9 @@ bounce	stx .xReg	;save the contents of the x-register
 .yReg	.DB 0
 
 ;	
-;sub-routine to get a char. Argument order is row, column
-;Parameters: row, column
-;Return: 1 byte (character at row,col); 00 if char out of bounds	
+; sub-routine to get a char. Argument order is row, column
+; Parameters: row, column
+; Return: 1 byte (character at row,col); 00 if char out of bounds	
 ;	
 getC	stx .xReg	;save the contents of the x-register
 	sty .yReg	;save the contents of the y-register
@@ -210,11 +199,11 @@ getC	stx .xReg	;save the contents of the x-register
 	pha		;store the col parameter
 	jsr onScrn	;check the parameters
 	pla		;get the return value
-	cmp #false	; if this is not false...
-	bne .cont	; carry on
-	lda #00		; return 00 (this line is only called on false)
+	cmp #false	;if this is not false...
+	bne .cont	;carry on
+	lda #00		;return 00 (this line is only called on false)
 	pha
-	jmp .return	; end the function prematurely (this line is only called on false)
+	jmp .return	;end the function prematurely (this line is only called on false)
 .cont	lda #$D8	;load 40 back from home
 	sta curLine
 	lda #$6F
@@ -227,11 +216,12 @@ getC	stx .xReg	;save the contents of the x-register
 	lda curLine+1	;deal with over flow
 	adc #00
 	sta curLine+1
+	cpx #$00
+	beq .getCh
 	dex
-	cpx #00
-	bne .getRow
+	jmp .getRow
 	;get the char
-	lda (curLine),y
+.getCh	lda (curLine),y
 	pha		;save the char
 	; return information
 .return	lda .save+1
@@ -253,9 +243,9 @@ getC	stx .xReg	;save the contents of the x-register
 printC	stx .xReg	;save the contents of the x-register
 	sty .yReg	;save the contents of the y-register
 	pla
-	sta .save	; where does .save and .save+1 actually save? ;save the first byte of the return address
+	sta .save	;where does .save and .save+1 actually save? ;save the first byte of the return address
 	pla
-	sta .save+1	; save the second byte of the return address
+	sta .save+1	;save the second byte of the return address
 	pla		;get column
 	tay		;save
 	pla		;get row
@@ -283,11 +273,12 @@ printC	stx .xReg	;save the contents of the x-register
 	lda curLine+1	;deal with over flow
 	adc #00
 	sta curLine+1
+	cpx #$00
+	beq .prChr
 	dex
-	cpx #00
-	bne .getRow
+	jmp .getRow
 	;print char
-	pla 		;get the char from the stack
+.prChr	pla 		;get the char from the stack
 	sta (curLine),y	;print the char
 .return	lda .save+1	;Restore return address upper byte
 	pha
@@ -319,12 +310,12 @@ onScrn	stx .xReg	;save the contents of the x-register
 	clv
 	cpx #00		;compare row to upper-most row
 	bmi .retFl	;if row<0, return false
-	cpx #25		;compare row to lower-most row + 1 (bpl is >=)
-	bpl .retFl	;if row>=25, return false
+	cpx #24		;compare row to lower-most row + 1 (bpl is >=)
+	bpl .retFl	;if row>=24, return false
 	cpy #00		;compare col to left-most col
 	bmi .retFl	;if col<0, return false
-	cpy #41		;compare col to right-most col
-	bpl .retFl	;if col>=41, return false
+	cpy #40		;compare col to right-most col
+	bpl .retFl	;if col>=40, return false
 .retTr	lda #true	;put return value on stack
 	pha
 	jmp .return	;return the function
@@ -347,9 +338,9 @@ onScrn	stx .xReg	;save the contents of the x-register
 ; Parameters: none
 ; Return: none
 ;
-crsrOff	lda #10         ; First byte links second byte to a specific crtc control register
+crsrOff	lda #10         ;First byte links second byte to a specific crtc control register
         sta $9000
-        lda #%00001000  ; Disable cursor with 5th bit 1 and 6th bit 0
+        lda #%00001000  ;Disable cursor with 5th bit 1 and 6th bit 0
         sta $9001
 	rts
 
@@ -359,7 +350,8 @@ crsrOff	lda #10         ; First byte links second byte to a specific crtc contro
 ; Return: none		
 ;	
 clrScrn	stx .xReg	;save the contents of the x-register
-	sty .yReg	;save the contents of the y-registerldx #0		;clear the x register
+	sty .yReg	;save the contents of the y-register
+	ldx #0		;clear the x register
 .nextLn	lda #space	;put space in the a register
 	ldy #0
 .loop	sta (curLine),y	;Print the space
